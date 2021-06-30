beb600ff51917fc06fe04b478e6e45e19c28568a
Galeforse <gabrielgrnt@googlemail.com>
2021-06-30 14:17:06 +0100

    Testing for speed-up :speedboat:

--- Gabriel/EDA/test.py
+++ Gabriel/EDA/test.py
@@ -1,5 +1,8 @@
 import modin.pandas as pd
 from datetime import datetime as dt
+from distributed import Client
+
+client = Client()
 
 start = dt.now()
 df = pd.read_csv("https://www.dropbox.com/s/c065rcq72abzm0s/Authentication_data.gz?dl=1",compression="gzip")
